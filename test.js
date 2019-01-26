d981efef8f50516afd6e245ca5836cf5ffe64d4d
Sharhzad <shahrzadnazemi.sn@gmail.com>
2019-01-26 14:26:23 +0330

    d

---
+++ test.js
@@ -0,0 +1,9 @@
+let db = require('./database/database')
+
+let lsnId = "5c28a3b8f8b2af36c96eee46"
+// db.getNextLesson(lsnId , (lesson)=>{
+//     console.log(lesson)
+// })
+var moment = require('moment')
+
+console.log(moment(1548064861590+ 60 * 60 * 24 * 1000))
