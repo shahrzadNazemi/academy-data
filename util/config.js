5d437a559361f5f91a39192028ba4a11aba0ba64
Sharhzad <shahrzadnazemi.sn@gmail.com>
2019-05-19 12:40:04 +0430

    d

--- util/config.js
+++ util/config.js
@@ -1,4 +1,4 @@
 
 // let password =encodeURIComponent( "rahasaRahasa")
-module.exports.mongoURL = `mongodb://adminUser:rahasaRahasa@127.0.0.1:27017/englishAcademy?authSource=admin`;
+module.exports.mongoURL = `mongodb://adminUser:rahasaRahasa@185.94.99.69:27017/englishAcademy?authSource=admin`;
     // mongodb://username:password@localhost:27017/exampledatabase
